23e0fe1a2d13da75f1f54d4848f8e2953d6216ad
Dmitriy Tereschenko <dmitriy.tereshchenko1989@gmail.com>
2024-05-07 23:04:30 +0300

    change url validation

--- UrlSaver/src/UrlSaver.Api/Extentions/UrlValidationExtention.cs
+++ UrlSaver/src/UrlSaver.Api/Extentions/UrlValidationExtention.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Text.RegularExpressions;
-
-namespace UrlSaver.Api.Extentions
+﻿namespace UrlSaver.Api.Extentions
 {
     public static class UrlValidationExtention
     {
-        private const string pattern =
-            @"^*((https?|ftps?):\/\/)?(?:[a-zA-Z0-9]" +
-            @"(?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}" +
-            @"(?::(?:0|[1-9]\d{0,3}|[1-5]\d{4}|6[0-4]\d{3}" +
-            @"|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?" +
-            @"(?:\/(?:[-a-zA-Z0-9@%_\+.~#?&=]+\/?)*)?$";
         public static bool ValidateUrl(this string url)
         {
-            var urlRegex = new Regex(pattern, RegexOptions.IgnoreCase);
-            return urlRegex.IsMatch(Uri.UnescapeDataString(url));
+            url = Uri.UnescapeDataString(url);
+            if (url.StartsWith("www."))
+            {
+                url = string.Concat("https://", url);
+            }
+
+            return Uri.TryCreate(Uri.UnescapeDataString(url), UriKind.Absolute, out Uri uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps ||
+                    uriResult.Scheme == Uri.UriSchemeFtp || uriResult.Scheme == Uri.UriSchemeFtps);
         }
     }
 }
