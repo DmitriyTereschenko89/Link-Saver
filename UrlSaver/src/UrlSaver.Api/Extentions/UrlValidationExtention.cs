f4e0943c446aa393edf229e010904e80fb47f823
Dmitriy Tereschenko <dmitriy.tereshchenko1989@gmail.com>
2024-05-07 19:52:18 +0300

    change url validator

--- UrlSaver/src/UrlSaver.Api/Extentions/UrlValidationExtention.cs
+++ UrlSaver/src/UrlSaver.Api/Extentions/UrlValidationExtention.cs
@@ -5,10 +5,16 @@ namespace UrlSaver.Api.Extentions
 {
     public static class UrlValidationExtention
     {
+        private const string pattern =
+            @"^*((https?|ftps?):\/\/)?(?:[a-zA-Z0-9]" +
+            @"(?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}" +
+            @"(?::(?:0|[1-9]\d{0,3}|[1-5]\d{4}|6[0-4]\d{3}" +
+            @"|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?" +
+            @"(?:\/(?:[-a-zA-Z0-9@%_\+.~#?&=]+\/?)*)?$";
         public static bool ValidateUrl(this string url)
         {
-            return Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult) &&
-                    (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            var urlRegex = new Regex(pattern, RegexOptions.IgnoreCase);
+            return urlRegex.IsMatch(Uri.UnescapeDataString(url));
         }
     }
 }
