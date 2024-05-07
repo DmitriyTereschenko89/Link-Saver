89990d32915c701f08f81aa97b17b130b3c7336e
Dmitriy Tereschenko <dmitriy.tereshchenko1989@gmail.com>
2024-05-07 23:24:33 +0300

    add additional url validation options

--- UrlSaver/src/UrlSaver.Api/Extentions/UrlValidationExtention.cs
+++ UrlSaver/src/UrlSaver.Api/Extentions/UrlValidationExtention.cs
@@ -10,9 +10,12 @@
                 url = string.Concat("https://", url);
             }
 
-            return Uri.TryCreate(Uri.UnescapeDataString(url), UriKind.Absolute, out Uri uriResult)
-                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps ||
-                    uriResult.Scheme == Uri.UriSchemeFtp || uriResult.Scheme == Uri.UriSchemeFtps);
+            return Uri.TryCreate(Uri.UnescapeDataString(url), UriKind.Absolute, out Uri uriResult) &&
+                    (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps ||
+                    uriResult.Scheme == Uri.UriSchemeFtp || uriResult.Scheme == Uri.UriSchemeFtps) &&
+                    uriResult.Host.Replace("www.", "").Split('.').Count() > 1 &&
+                    uriResult.HostNameType == UriHostNameType.Dns &&
+                    uriResult.Host.Length > uriResult.Host.LastIndexOf(".") + 1;
         }
     }
 }
